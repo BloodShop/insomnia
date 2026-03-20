0c154b9899ff8e191babf95174952fc2447e8960
Bingbing <ZxBing0066@gmail.com>
2026-03-20 16:56:31 +0800

    feat: migrate models to insomnia-data - p3

--- packages/insomnia/src/insomnia-data/node-src/services/index.ts
+++ packages/insomnia/src/insomnia-data/node-src/services/index.ts
@@ -1,13 +1,25 @@
+import * as apiSpecService from './api-spec';
 import * as caCertificateService from './ca-certificate';
+import * as clientCertificateService from './client-certificate';
+import * as cloudCredentialService from './cloud-credential';
+import * as gitCredentialsService from './git-credentials';
+import * as gitRepositoryService from './git-repository';
 import * as mcpPayloadService from './mcp-payload';
 import * as mcpRequestService from './mcp-request';
 import * as mcpResponseService from './mcp-response';
+import * as runnerTestResultService from './runner-test-result';
 import * as settingsService from './settings';
 
 export const servicesNodeImpl = {
+  apiSpec: apiSpecService,
   caCertificate: caCertificateService,
+  clientCertificate: clientCertificateService,
+  cloudCredential: cloudCredentialService,
+  gitCredentials: gitCredentialsService,
+  gitRepository: gitRepositoryService,
+  mcpPayload: mcpPayloadService,
   mcpRequest: mcpRequestService,
   mcpResponse: mcpResponseService,
-  mcpPayload: mcpPayloadService,
+  runnerTestResult: runnerTestResultService,
   settings: settingsService,
 } as const;
